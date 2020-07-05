06a8c38183e98eb09bcb6bbfea95c03cb238108c
DhruvaSuman <kumarsuman99340@gmail.com>
2020-07-05 06:39:01 +0530

    My first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+DhruvaHousePredictor
+DhruvaHousePredictor
---
+++ website/predict.php
@@ -0,0 +1,23 @@
+<?php
+//Receive get Request
+$lotsize = $_GET["lotsize"];
+$bathrms = $_GET["bathrms"]; 
+$stories = $_GET["stories"];
+$driveway = $_GET["driveway"];
+$recroom = $_GET["recroom"];
+$fullbase = $_GET["fullbase"];
+$gashw = $_GET["gashw"];
+$airco = $_GET["airco"];
+$garagepl = $_GET["garagepl"];
+$prefarea = $_GET["prefarea"];
+
+
+// Execute a system command
+system("/home/anaconda/bin/python3 model_house.py ".$lotsize." ".$bathrms." 
+".$stories." ".$driveway."  ".$recroom." 
+".$fullbase." ".$gashw." ".$airco." 
+".$garagepl." ".$prefarea." 2>&1");
+?>
+
+
+
